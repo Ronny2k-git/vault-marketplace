c5cd86ca5ebf3d071a1613cc4c3a733c8a8a05af
Ronny2k-git <ronnyedson11@gmail.com>
2025-01-19 10:07:08 -0300

    Vault-tokens

--- src/components/vault/vaultCardCreate.tsx
+++ src/components/vault/vaultCardCreate.tsx
@@ -7,17 +7,22 @@ export default function CardCreate() {
     <div>
       <Card intent={"primary"} size={"small"}>
         <div className="relative w-full h-fit aspect-video overflow-hidden flex-grow-0">
-          <img
-            className="rounded-t-xl size-full object-cover opacity-40"
-            src="/backgroundCard.png"
+          <div
+            className="rounded-t-xl h-36 w-full object-cover opacity-30
+          bg-[url(/backgroundCard.png)]"
           />
-          {/* <img className="size-16 absolute" src="/icons/iconFrame.png" /> */}
           <div className="flex z-40 bottom-2 gap-2 left-2 absolute">
             <img className="size-8 mt-1" src="/icons/frame.png" />
             <div className=" font-normal text-white text-base">
               Unnamed
               <br />
               <div className="text-[10px] -mt-2">No chain selected</div>
+            </div>
+            <div className="-ml-14 opacity-45 ">
+              <img
+                className="size-16 absolute bottom-9"
+                src="/icons/iconFrame.png"
+              />
             </div>
           </div>
         </div>
