06e7258e74d3ece1618c9db1ffde414a6b316767
Ronny2k-git <ronnyedson11@gmail.com>
2025-02-01 13:19:12 -0300

    Vault-create

---
+++ src/app/api/create/createPrisma.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const createVaultInDb = async (vaultDate: any) => {
+  try {
+    const {
+      address,
+      vaultName,
+      vaultLogo,
+      bannerUrl,
+      startDate,
+      endDate,
+      chainId,
+      assetTokenDecimals,
+      assetTokenName,
+      assetTokenSymbol,
+    } = vaultDate;
+
+    const vault = await prisma.vault.create({
+      data: {
+        address,
+        name: vaultName,
+        logo: vaultLogo,
+        banner: bannerUrl,
+        startsAt: startDate,
+        endsAt: endDate,
+        chainId,
+        assetTokenDecimals,
+        assetTokenName,
+        assetTokenSymbol,
+      },
+    });
+    return vault;
+  } catch (error) {
+    console.error("Error saving vault to DB");
+    throw new Error("Failed to save vault to database");
+  }
+};
