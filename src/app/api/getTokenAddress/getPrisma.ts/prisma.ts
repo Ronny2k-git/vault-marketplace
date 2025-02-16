1c96b4ba23b6f7b023cf4f2800bf2eca900f829b
Ronny2k-git <ronnyedson11@gmail.com>
2025-02-16 10:50:39 -0300

    Vault-timer

--- src/app/api/getTokenAddress/getPrisma.ts/prisma.ts
+++ src/app/api/getTokenAddress/getPrisma.ts/prisma.ts
@@ -26,3 +26,41 @@ export const getVaultInDb = async () => {
     );
   }
 };
+
+// import { PrismaClient } from "@prisma/client";
+// import { NextResponse } from "next/server";
+
+// const prisma = new PrismaClient();
+
+// export const getVaultInDb = async (address: string) => {
+//   try {
+//     const vault = await prisma.vault.findUnique({
+//       where: {
+//         address: address, // Busca o vault pelo endereço
+//       },
+//       select: {
+//         id: true,
+//         name: true,
+//         address: true,
+//         logo: true,
+//         banner: true,
+//         startsAt: true,
+//         endsAt: true,
+//         assetTokenName: true,
+//         assetTokenAddress: true,
+//       },
+//     });
+
+//     // Verifica se o vault foi encontrado
+//     if (!vault) {
+//       throw new Error("Vault not found");
+//     }
+
+//     return vault;
+//   } catch (error) {
+//     return NextResponse.json(
+//       { success: false, message: error || "Error fetching vault data" },
+//       { status: 500 }
+//     );
+//   }
+// };
--- src/app/api/getTokenAddress/route.ts
+++ src/app/api/getTokenAddress/route.ts
@@ -15,3 +15,28 @@ export async function GET(req: NextRequest) {
     }
   }
 }
+
+// import { NextRequest, NextResponse } from "next/server";
+// import { getVaultInDb } from "./getPrisma.ts/prisma";
+
+// export async function GET(req: NextRequest) {
+//   try {
+//     const { searchParams } = new URL(req.url);
+//     const address = searchParams.get("address");
+
+//     if (!address) {
+//       return NextResponse.json(
+//         { success: false, message: "Address parameter is required" },
+//         { status: 400 }
+//       );
+//     }
+//     const vault = await getVaultInDb(address);
+
+//     return NextResponse.json({ success: true, vault }, { status: 200 });
+//   } catch (error) {
+//     return NextResponse.json(
+//       { success: false, message: error || "Error fetching vault data" },
+//       { status: 500 }
+//     );
+//   }
+// }
