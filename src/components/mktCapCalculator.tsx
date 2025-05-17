f60de0d48733dea35ff977cccaa777623ab726a2
Ronny2k-git <ronnyedson11@gmail.com>
2025-05-17 15:00:55 -0300

    Chore: Add new functionalities to the mkt cap page.

--- src/components/mktCapCalculator.tsx
+++ src/components/mktCapCalculator.tsx
@@ -3,10 +3,12 @@
 import { useState } from "react";
 import { LuDollarSign } from "react-icons/lu";
 import { GiToken } from "react-icons/gi";
+import { MdOutlineDriveFileRenameOutline } from "react-icons/md";
 
 export function MktCapCalculator() {
   const [currentPrice, setCurrentPrice] = useState("");
   const [circulatingSupply, setCirculatingSupply] = useState("");
+  const [tokenName, setTokenName] = useState("");
 
   const handlePriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value.replace(/,/g, "");
@@ -35,8 +37,23 @@ export function MktCapCalculator() {
   return (
     <div className="flex justify-between">
       <div className="flex flex-col gap-2 relative">
+        <h3 className="text-white">Token name</h3>
+        <MdOutlineDriveFileRenameOutline
+          className="size-5 absolute mt-9 ml-2"
+          color="white"
+        />
+        <input
+          className="h-8 max-w-md bg-gray-400 rounded-lg px-8 focus:outline-none focus:ring-2 focus:ring-gray-200"
+          placeholder="Ex: 1.20"
+          type="text"
+          onChange={(e) => setTokenName(e.target.value)}
+          value={tokenName}
+        />
         <h3 className="text-white">Token Current price $</h3>
-        <LuDollarSign className="size-5 absolute mt-9 ml-2" color="white" />
+        <LuDollarSign
+          className="size-5 absolute mt-[108px] ml-2"
+          color="white"
+        />
         <input
           className="h-8 max-w-md bg-gray-400 rounded-lg px-8 focus:outline-none focus:ring-2 focus:ring-gray-200"
           placeholder="Ex: 1.20"
@@ -45,14 +62,20 @@ export function MktCapCalculator() {
         />
 
         <h3 className="text-white">Token circulating supply</h3>
-        <GiToken className="size-5 absolute mt-28 ml-2" color="white" />
+        <GiToken className="size-5 absolute mt-[183px] ml-2" color="white" />
         <input
           className="h-8 max-w-md bg-gray-400 rounded-lg px-8 focus:outline-none focus:ring-2 focus:ring-gray-200"
           placeholder="Ex: 1,000,000"
           value={circulatingSupply}
           onChange={handleSupplyChange}
         />
-        <h3 className="text-white">Then its market cap is:</h3>
+        <h3 className="flex gap-2 text-white">
+          So the{" "}
+          <p className="text-green-500 underline">
+            {tokenName || "token name"}
+          </p>
+          market cap is:
+        </h3>
         <div className="h-12 max-w-md text-green-500 flex justify-center items-center bg-gray-900 rounded-lg px-5">
           {`$ ${marketCap.toLocaleString("en-US", {
             maximumFractionDigits: 2,
