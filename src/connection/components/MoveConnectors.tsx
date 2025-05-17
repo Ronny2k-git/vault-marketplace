615472472f92abe7a2c3d0d65870ad41bfb44d6f
Ronny2k-git <ronnyedson11@gmail.com>
2025-05-17 16:36:39 -0300

    Feat: add a new ecosystem to the website

--- src/connection/components/MoveConnectors.tsx
+++ src/connection/components/MoveConnectors.tsx
@@ -1,13 +1,62 @@
+import { Button } from "@/components/interface/button";
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import Image from "next/image";
+
 export function MoveConnectors() {
+  const { account, wallets, connect, disconnect, connected, isLoading } =
+    useWallet();
+
+  const abreviateAddress = (address?: string) => {
+    if (!address) return "";
+    return `${address.slice(0, 7)}...${address.slice(-5)}`.toLowerCase();
+  };
+
   return (
-    <div>
+    <div className="flex flex-col gap-2.5">
       <div className="flex justify-between gap-6 mb-2">
         <h1>Move Connectors</h1>
         <div className="text-sm flex items-center text-black bg-white rounded-full px-2">
-          Not connected
+          {!connected
+            ? "Not connected"
+            : `${abreviateAddress(account?.address?.toString())}`}
         </div>
       </div>
-      Tesssssssssssstttttttttttttttttttttttttttt
+      {connected ? (
+        <div className="flex justify-between bg-gray-glow rounded-2xl py-2 px-2">
+          <Image
+            src="/connectorIcons[wallet!.adapter.name]"
+            width={26}
+            height={20}
+            alt="user icon"
+            className="rounded-full"
+          />
+          <button
+            className="h-8 w-8 rounded-full text-sm text-black px-2 bg-gray-100"
+            onClick={() => disconnect()}
+          >
+            disconnect
+          </button>
+        </div>
+      ) : (
+        wallets.map((connector) => (
+          <Button
+            className="flex"
+            key={connector.name}
+            size="medium"
+            intent="neutral"
+            onClick={() => connect(connector.name)}
+          >
+            <Image
+              width={24}
+              height={24}
+              alt="connector icon"
+              className="rounded-full mr-1"
+              src="/usdc.png"
+            />
+            {connector.name}
+          </Button>
+        ))
+      )}
     </div>
   );
 }
