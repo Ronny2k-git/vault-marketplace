6e768960d043b0b452a73eac7151b9843e92cb39
Ronny2k-git <ronnyedson11@gmail.com>
2025-05-20 15:02:04 -0300

    Chore: Fix countdown timer into token page id.

---
+++ src/components/countDownClock.tsx
@@ -0,0 +1,92 @@
+"use client";
+
+import { vaultAtom } from "@/utils/atom";
+import { useAtom } from "jotai";
+import { useEffect, useState } from "react";
+
+export function CountDownClock() {
+  const [vaultData] = useAtom(vaultAtom);
+  const [timeLeft, setTimeLeft] = useState<number>(0);
+
+  const getStatus = () => {
+    const currentDate = new Date();
+    const startDate = new Date(vaultData.startsAt);
+    const endDate = new Date(vaultData.endsAt);
+
+    if (!vaultData) {
+      return "No vault data";
+    }
+
+    if (startDate > currentDate) {
+      return "Coming soon";
+    }
+    if (startDate < currentDate && currentDate < endDate) {
+      return "Live";
+    } else {
+      return "Finished";
+    }
+  };
+
+  const calculateTimeLeft = () => {
+    const currentDate = new Date();
+    const startDate = new Date(vaultData.startsAt);
+    const endDate = new Date(vaultData.endsAt);
+
+    if (!vaultData) return;
+
+    if (getStatus() === "Coming soon") {
+      setTimeLeft(startDate.getTime() - currentDate.getTime());
+    } else if (getStatus() === "Live") {
+      setTimeLeft(endDate.getTime() - currentDate.getTime());
+    } else {
+      setTimeLeft(0);
+    }
+  };
+
+  //Math.floor rounds a number down.
+
+  const days = Math.max(Math.floor(timeLeft / 1000 / 60 / 60 / 24), 0);
+  const hours = Math.max(Math.floor(timeLeft / 1000 / 60 / 60) % 24, 0);
+  const minutes = Math.max(Math.floor(timeLeft / 1000 / 60) % 60, 0);
+  const seconds = Math.max(Math.floor((timeLeft / 1000) % 60), 0);
+
+  //The padStart() is used to add characters to the left of a string until it reaches a specified length
+
+  const twoDigits = (n: number) => n.toString().padStart(2, "0");
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      calculateTimeLeft();
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [vaultData]);
+
+  return (
+    <div className="flex flex-col ml-16">
+      <div className="flex max-sm:flex-col gap-2 sm:gap-6 justify-between sm:items-center"></div>
+      <div className="flex text-live-accent text-base">
+        <div className="flex flex-col items-center mr-1">
+          <span>{twoDigits(days)}</span>
+          <span className="text-white text-xs">day</span>
+        </div>
+        <span>/</span>
+        <div className="flex flex-col items-center">
+          <span>{twoDigits(hours)}</span>
+          <span className="text-white text-xs">hour</span>
+        </div>
+        <span>:</span>
+        <div className="flex flex-col items-center">
+          <span>{twoDigits(minutes)}</span>
+          <span className="text-white text-xs">min</span>
+        </div>
+        <span>:</span>
+
+        <div className="flex flex-col items-center">
+          <span>{twoDigits(seconds)}</span>
+          <span className="text-white text-xs">sec</span>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/vault/vaultCardTransaction.tsx
+++ src/components/vault/vaultCardTransaction.tsx
@@ -6,20 +6,19 @@ import { CardDeposit } from "./vaultCardDeposit";
 import { CardRemove } from "./vaultCardRemove";
 import { vaultAtom } from "@/utils/atom";
 import { useAtom } from "jotai";
-import { useEffect, useState } from "react";
+import { CountDownClock } from "../countDownClock";
 
 export function CardTransaction() {
   const [vaultData] = useAtom(vaultAtom);
-  const [timeLeft, setTimeLeft] = useState<string>("00:00:00");
 
   const getStatus = () => {
-    if (!vaultData) {
-      return "No vault data";
-    }
-
     const currentDate = new Date();
     const startDate = new Date(vaultData.startsAt);
     const endDate = new Date(vaultData.endsAt);
+
+    if (!vaultData) {
+      return "No vault data";
+    }
 
     if (startDate > currentDate) {
       return "Coming soon";
@@ -31,50 +30,10 @@ export function CardTransaction() {
     }
   };
 
-  const calculateTimeLeft = () => {
-    if (!vaultData) return;
-
-    let timeDiff: number;
-
-    const currentDate = new Date();
-    const startDate = new Date(vaultData.startsAt);
-    const endDate = new Date(vaultData.endsAt);
-
-    if (getStatus() === "Coming soon") {
-      timeDiff = startDate.getTime() - currentDate.getTime(); //The getTime function gets the difference in milliseconds
-    } else if (getStatus() === "Live") {
-      timeDiff = endDate.getTime() - currentDate.getTime();
-    } else {
-      return;
-    }
-
-    //Math.floor rounds a number down.
-
-    const hours = Math.floor(timeDiff / (1000 * 60 * 60));
-    const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
-
-    //The padStart() is used to add characters to the left of a string until it reaches a specified length
-
-    setTimeLeft(
-      `${hours.toString().padStart(2, "0")}:${minutes
-        .toString()
-        .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`
-    );
-  };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      calculateTimeLeft();
-    }, 1000);
-
-    return () => clearInterval(interval);
-  }, [vaultData]);
-
   return (
     <div>
       <Card className="flex flex-col" intent={"secondary"} size={"mediumLarge"}>
-        <h1 className="mt-4 ml-4 flex text-sm text-text-foreground">
+        <div className="px-4 py-2 flex items-center text-sm text-text-foreground">
           Status:
           <p
             className={
@@ -85,8 +44,9 @@ export function CardTransaction() {
           >
             {getStatus()}
           </p>
-        </h1>
-        <div className="ml-4 text-base mb-2 text-live-accent">{timeLeft}</div>
+          <CountDownClock />
+        </div>
+
         <Card className="flex flex-col" intent={"primary"} size={"medium"}>
           <Tabs.Root defaultValue="Deposit">
             <Tabs.List
